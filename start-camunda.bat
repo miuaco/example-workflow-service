8c04a37ddd919ba5e694ec7408a5267296224d2d
Shuichi Miura <shuichi.miura@s1products.info>
2021-06-23 10:58:42 +0900

    Change JarLauncher to PropertiesLauncher in pom.xml. And change -Dloader.path.

--- start-camunda.bat
+++ start-camunda.bat
@@ -4,4 +4,4 @@ call mvnw install
 cd ../example-workflow-service
 call mvnw install
 
-call java -classpath ./target/example-workflow-service-0.0.1-SNAPSHOT.jar;./extlibs/* org.springframework.boot.loader.JarLauncher
+call java -Dloader.path=extlibs/* -jar target/example-workflow-service-0.0.1-SNAPSHOT.jar
